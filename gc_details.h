6e0670fc2765d576a64a95d68545c4a2e60acace
TomTheToad <tomthetoad951@gmail.com>
2019-07-24 18:24:41 -0600

    Code cleanup and PtrDetails updates. PtrDetails maybe complete from older build

--- gc_details.h
+++ gc_details.h
@@ -8,26 +8,28 @@ class PtrDetails
     unsigned refcount; // current reference count
     T *memPtr;         // pointer to allocated memory
     /* isArray is true if memPtr points
-to an allocated array. It is false
-otherwise. */
+    to an allocated array. It is false
+    otherwise. */
     bool isArray; // true if pointing to array
     /* If memPtr is pointing to an allocated
-array, then arraySize contains its size */
+    array, then arraySize contains its size */
     unsigned arraySize; // size of array
     // Here, mPtr points to the allocated memory.
     // If this is an array, then size specifies
     // the size of the array.
 
-    PtrDetails(void)
-    {
-        // TODO: Implement PtrDetails
+    // PtrDetails(void)
+    PtrDetails(T *memPtr, unsigned arraySize = 0) {
+        this->memPtr = memPtr;
+        this->arraySize = arraySize;
+        if (this->arraySize > 0) {
+        this->isArray = true;
+        }
     }
 };
 // Overloading operator== allows two class objects to be compared.
 // This is needed by the STL list class.
 template <class T>
-bool operator==(const PtrDetails<T> &ob1,
-                const PtrDetails<T> &ob2)
-{
-    // TODO: Implement operator==
+bool operator==(const PtrDetails<T> &ob1, const PtrDetails<T> &ob2) {
+    return ((ob1.memPtr == obj2.memPtr) ? true : false);
 }
