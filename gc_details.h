346a20f485e82e7611c4092d752331bfbb613d7d
TomTheToad <tomthetoad951@gmail.com>
2019-08-05 10:08:11 -0400

    type: refactor
    task: Removed / shortened code. Is possible that isArray was not getting properly set.

--- gc_details.h
+++ gc_details.h
@@ -22,14 +22,12 @@ class PtrDetails
     PtrDetails(T *memPtr, unsigned arraySize = 0) {
         this->memPtr = memPtr;
         this->arraySize = arraySize;
-        if (this->arraySize > 0) {
-        this->isArray = true;
-        }
+        this->isArray = (arraySize > 0) ? true : false;
     }
 };
 // Overloading operator== allows two class objects to be compared.
 // This is needed by the STL list class.
 template <class T>
 bool operator==(const PtrDetails<T> &ob1, const PtrDetails<T> &ob2) {
-    return ((ob1.memPtr == ob2.memPtr) ? true : false);
+    return (ob1.memPtr == ob2.memPtr);
 }
